eb11fe7949f0daee0442900410e3630d9b7bf3dd
Yiğit Kemal Erinç <yigit.erinc@ozu.edu.tr>
2020-02-29 00:34:09 +0300

    Create page UI

--- qrpaybank/App.js
+++ qrpaybank/App.js
@@ -1,19 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { Container, Header, Content, Input, Item, Body, Left, Button, Right, Title, Icon } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+      <Container>
+        <Header>
+          <Left/>
+            <Body>
+              <Title>QR Pay</Title>
+            </Body>
+          <Right/>
+        </Header>
+
+        <Content style={styles.content}>
+          <Text style={styles.greeting}>QR-Odeme - Happy Moons Fenerbahce</Text>
+          <Text style={styles.price}>Price</Text>
+          <Item regular style={styles.priceBox}>
+            <Input placeholder='Price'/>
+          </Item>
+          <Button primary style={styles.buttonStyle}>
+            <Text style={styles.buttonText}>QR Oluştur</Text>
+          </Button>
+        </Content>
+      </Container>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    marginTop: Dimensions.get('window').height * 15 / 100,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+
+  greeting: {
+    marginTop: Dimensions.get('window').height * 5 / 100,
+    marginBottom: Dimensions.get('window').height * 5 / 100,
+    fontSize: 21
+  },
+
+  buttonText: {
+    textAlign: 'center',
+    alignContent: 'center',
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+
+  buttonStyle: {
+    flex: 1, 
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center', 
+    width: 100,
+    marginLeft: Dimensions.get('window').width * 56 / 100
+  },
+
+  content: {
+    marginLeft: Dimensions.get('window').width * 5 / 100,
+    marginRight: Dimensions.get('window').width * 5 / 100
+  },
+  
+  price: {
+    fontSize: 20,
+    marginLeft: 5,
+    marginBottom: 3
+  },
+
+  priceBox: {
+    width: Dimensions.get('window').width * 80 / 100
+  }
 });
