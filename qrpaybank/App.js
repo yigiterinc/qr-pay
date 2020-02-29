93a714b1af6f0250c9954d06f9595ef1e0597ce3
barisconur <barisconur@gmail.com>
2020-02-29 13:27:09 +0300

    qr generator update

--- qrpaybank/App.js
+++ qrpaybank/App.js
@@ -17,58 +17,61 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <KeyboardAvoidingView  keyboardVerticalOffset = {Header.HEIGHT + 20}
-      style = {{flex: 1}}
-      behavior = "padding">
-        <Container>
-          <Header>
-            <Left/>
-              <Body>
-                <Title>QR Pay</Title>
-              </Body>
-            <Right/>
-          </Header>
+      <Container>
+        <Header>
+          <Left/>
+            <Body>
+              <Title>QR Pay</Title>
+            </Body>
+          <Right/>
+        </Header>
 
-          <Content style={styles.content}>
-            <Text style={styles.greeting}>QR-Odeme - Happy Moons Fenerbahce</Text>
+        <Content style={styles.content}>
+          <Text style={styles.greeting}>QR-Odeme - Happy Moons Fenerbahce</Text>
 
-            <Text style={styles.price}>Price</Text>
+          <View style={styles.priceFieldContainer}>
             <Item regular style={styles.priceBox}>
               <Input placeholder='Price' onChangeText={text => this.onChangeText(text)}/>
             </Item>
-
+          </View>
+        
+          <View style={styles.buttonContainer}>
             <Button primary style={styles.buttonStyle} onPress={() =>  {this.setState({showQR: true})}}>
               <Text style={styles.buttonText}>QR Oluştur</Text>
             </Button>
-
-            {this.state.showQR && (
-                  <QRCode
-                    value={this.state.price}
-                    style={{position: 'absolute', bottom: 0, marginTop: 100, marginLeft: 50}}
-                    //Setting the value of QRCode
-                    size={250}
-                    //Size of QRCode
-                    bgColor="#000"
-                    //Backgroun Color of QRCode
-                    fgColor="#fff"
-                    //Front Color of QRCode
-              />
-              ) 
-              }
-          </Content>
-        </Container>
-      </KeyboardAvoidingView>
-
-  )
+          </View>
+        
+          <View style={styles.qrContainer}>
+            { this.displayQR() }
+          </View>
+          
+          
+        </Content>
+      </Container>
+    )
   }
 
   onChangeText = (text) => {
-    if (!this.state.showQR) {
+    if (this.state.price !== 0) {
       this.setState({
-        price: text
+        price: text,
+        showQR: !this.state.showQR
       });
     }
   }
+
+  displayQR = () => {
+    if (this.state.showQR) {      
+      return   <QRCode
+      value={this.state.price}
+      size={250}
+      bgColor="#000"
+      fgColor="#fff"
+      />
+    }
+    return null
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -81,13 +84,15 @@ const styles = StyleSheet.create({
   greeting: {
     marginTop: Dimensions.get('window').height * 5 / 100,
     marginBottom: Dimensions.get('window').height * 5 / 100,
-    fontSize: 21
+    fontSize: 21,
+    paddingLeft: 20
   },
 
   buttonText: {
     textAlign: 'center',
     alignContent: 'center',
     fontSize: 15,
+    paddingLeft: 10,
     fontWeight: 'bold',
     color: 'white'
   },
@@ -96,14 +101,22 @@ const styles = StyleSheet.create({
     flex: 1, 
     marginTop: 10,
     flexDirection: 'row',
-    justifyContent: 'center', 
-    width: 100,
-    marginLeft: Dimensions.get('window').width * 56 / 100
+    width: 100
+  },
+
+  buttonContainer: {
+    flex:1,
+    alignItems:'center'
   },
 
   content: {
     marginLeft: Dimensions.get('window').width * 5 / 100,
     marginRight: Dimensions.get('window').width * 5 / 100
+  },
+
+  priceFieldContainer: {
+    flex:1,
+    alignItems:'center'
   },
   
   price: {
@@ -114,5 +127,11 @@ const styles = StyleSheet.create({
 
   priceBox: {
     width: Dimensions.get('window').width * 80 / 100
+  },
+
+  qrContainer: {
+    paddingTop: Dimensions.get('window').height * 7.5 / 100,
+    flex:1,
+    alignItems:'center'
   }
 });
